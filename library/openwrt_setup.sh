49334f3b5d4bcb531975162dbb8aedb9a8441bdc
gekmihesg <markus@gekmihesg.de>
2018-06-20 21:37:47 +0200

    add some well-known facts

--- library/openwrt_setup.sh
+++ library/openwrt_setup.sh
@@ -13,8 +13,33 @@ add_ubus_fact() {
 
 main() {
     ubus="/bin/ubus"
-    seperator=""
-    echo '{"changed":false,"ansible_facts":{'
+    seperator=","
+    echo '{"changed":false,"ansible_facts":'
+    dist="OpenWRT"
+    dist_version="NA"
+    dist_release="NA"
+    test -f /etc/openwrt_release && {
+        . /etc/openwrt_release
+        dist="${DISTRIB_ID:-$dist}"
+        dist_version="${DISTRIB_RELEASE:-$dist_version}"
+        dist_release="${DISTRIB_CODENAME:-$dist_release}"
+    } || test ! -f /etc/os-release || {
+        . /etc/os-release
+        dist="${NAME:-$dist}"
+        dist_version="${VERSION_ID:-$dist_version}"
+    }
+    dist_major="${dist_version%%.*}"
+    json_set_namespace facts
+    json_init
+    json_add_string ansible_distribution "$dist"
+    json_add_string ansible_distribution_major_version "$dist_major"
+    json_add_string ansible_distribution_release "$dist_release"
+    json_add_string ansible_distribution_version "$dist_version"
+    json_add_string ansible_os_family OpenWRT
+    dist_facts="$(json_dump)"
+    json_cleanup
+    json_set_namespace result
+    echo "${dist_facts%\}*}"
     for fact in \
             info/system/info \
             devices/network.device/status \
